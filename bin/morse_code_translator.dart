ab1e86feaa325eeb29edb419303c01e27739d8c3
Mirror83 <glenochieng045@gmail.com>
2024-04-23 18:23:46 +0300

    Added code to encode sentences and choose between encoding and translation.

--- bin/morse_code_translator.dart
+++ bin/morse_code_translator.dart
@@ -1,32 +1,99 @@
 import "package:morse_code_translator/lib.dart";
-import "dart:io" show stdin;
+import "dart:io" show exit, stdin;
+
+const translateMode = 1;
+const encodeMode = 2;
+const title = "Morse code translator and encoder.";
+const exitChar = "x";
 
 void main(List<String> arguments) {
   var morseCodeTree = MorseCodeTree();
 
-  const title = "Morse code translator.";
   print(title);
-  print("_" * title.length);
+  print("-" * title.length);
+
+  print("(Type $exitChar to quit)");
+
+  var mode = chooseMode();
 
   while (true) {
-    print("Enter the morse code sequence to translate (type x to quit):");
-    final input = stdin.readLineSync();
+    if (mode == translateMode) {
+      print("Enter the morse code sequence to translate:");
+      final input = readUserInput();
 
-    if (input == null) {
-      print("Invalid input.");
-      continue;
-    }
+      if (input == null) {
+        print("Invalid input.");
+        continue;
+      }
 
-    if (input == "x") {
-      print("Goodbye!");
+      final translation = morseCodeTree.translate(input);
+      if (translation == null) {
+        print("Invalid morse code sequence.");
+      } else {
+        print("Translation: $translation");
+      }
+    } else if (mode == encodeMode) {
+      print("Enter the sentence to translate (type 0 to exit):");
+      final input = readUserInput(exitChar: "0");
+
+      if (input == null) {
+        print("Invalid input.");
+        continue;
+      }
+
+      final codeSequence = morseCodeTree.encode(input);
+      if (codeSequence == null) {
+        print("Invalid sentence.");
+      } else {
+        print("Code sequence: $codeSequence");
+      }
+    } else {
       break;
     }
-
-    final translation = morseCodeTree.translate(input);
-    if (translation == null) {
-      print("Invalid morse code sequence.");
-    } else {
-      print("Translation: $translation");
-    }
   }
+}
+
+String? readUserInput({String exitChar = "x"}) {
+  final input = stdin.readLineSync()?.trim();
+
+  if (input == exitChar) {
+    print("Goodbye.");
+    exit(0);
+  }
+
+  return input;
+}
+
+int chooseMode() {
+  print("");
+  print("Choose a mode: ");
+  print("1. Translate");
+  print("2. Encode");
+
+  late int? mode;
+
+  do {
+    print("Choice: ");
+    var modeInput = readUserInput(exitChar: exitChar);
+
+    while (modeInput == null) {
+      print("Invalid input. Try again.");
+      print("Mode: ");
+      modeInput = readUserInput(exitChar: exitChar);
+    }
+
+    print("Chosen mode: $modeInput");
+
+    mode = int.tryParse(modeInput);
+
+    if (isModeInvalid(mode)) {
+      print("Please choose between 1 and 2 for the mode");
+    }
+  } while (isModeInvalid(mode));
+
+  return mode!;
+}
+
+bool isModeInvalid(int? mode) {
+  return mode == null || mode < translateMode || mode > encodeMode;
 }
